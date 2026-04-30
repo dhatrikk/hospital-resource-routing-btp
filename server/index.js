b8488276ea4b8dbc8062583218fb9b2222725cd9
Dhatrik <dhatrikkr@gmail.com>
2026-04-30 18:22:07 +0530

    cors improved

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import hospitalRoutes from "./routes/hospital.routes.js";
 const app =  express();
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://main.d2hq3ta0u12dgn.amplifyapp.com",
     credentials:true,
 }));
 dotenv.config();
