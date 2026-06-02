718a576398e9de8861303950be9f6bad8cf254d9
Dhatrik <dhatrikkr@gmail.com>
2026-06-02 16:15:22 +0530

    cors resolved

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://main.d33b18ju9ag449.amplifyapp.com/"
+    "https://main.d33b18ju9ag449.amplifyapp.com"
   ],
   credentials: true,
 }));
